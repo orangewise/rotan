b6f35ebffa3041cf4668b56568958779f0a554bf
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-10-09 17:05:48 +0200

    refactor fileContents

--- test/inject.js
+++ test/inject.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var inject = require('../lib/inject.js');
 var cors = require('./fixtures/cors.json');
 var swaggerParser = require('swagger-parser');
+var util = require('../lib/util.js');
 
 test('inject array of json object', function (t) {
   var injected = inject.array([ { a: 'b' }, { b: 'c' }]);
@@ -21,7 +22,7 @@ test('return folder as array', function (t) {
 test('openapi is still valid after injection', function (t) {
 
   inject.readFiles('./test/fixtures/inject/*.yaml', function (e, injectJson) {
-    inject.fileContents('./test/fixtures/swagger-base.yaml', function (e, api) {
+    util.fileContents('./test/fixtures/swagger-base.yaml', function (e, api) {
       // Add api to injectJson
       injectJson.push(api);
       var merged = inject.array(injectJson);
